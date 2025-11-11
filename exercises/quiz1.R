d47f0144e8e783abc8d304c5c7d72d7bf583d4c4
Florian Oswald <florian.oswald@gmail.com>
2025-11-11 22:35:37 +0100

    up pyton and r

--- exercises/quiz1.R
+++ exercises/quiz1.R
@@ -2,4 +2,4 @@ library(exams)
 
 exams2html(list.files(pattern = ".Rmd"))
 
-exams2moodle(list.files(pattern = ".Rmd"),nsamp = 3,edir = "moodles")
+exams2moodle(list.files(pattern = ".Rmd"),edir = "moodles")
